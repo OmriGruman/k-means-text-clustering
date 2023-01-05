97232a69ff0313c1f9e80b5a95d922f5d5eef06a
Omri Gruman <OmriGr1515@gmail.com>
2023-01-05 15:50:25 +0200

    Changed BERT model.

--- main.py
+++ main.py
@@ -9,39 +9,40 @@ from sentence_transformers import SentenceTransformer
 class KMeans:
     def __init__(self, n_clusters):
         self.n_clusters = n_clusters
+        self.centroids = []
+        self.clusters = []
 
-    def fit(self, X):
+    def fit(self, embeddings, max_iter=None):
         # initialize centroids randomly
-        self.centroids = X[np.random.choice(X.shape[0], self.n_clusters, replace=False)]
+        self.centroids = embeddings[np.random.choice(embeddings.shape[0], self.n_clusters, replace=False)]
 
         # initialize cluster assignments
-        self.clusters = np.zeros(X.shape[0])
+        self.clusters = np.zeros(embeddings.shape[0])
 
         # run kmeans until convergence
         while True:
             prev_clusters = self.clusters.copy()
 
-            # assign each point to closest centroid
-            for i, point in enumerate(X):
-                if type(point) != type(np.array([])):
-                    point = point.toarray()
-                distances = np.linalg.norm(point - self.centroids, axis=1)
-                self.clusters[i] = np.argmin(distances)
+            # update each embedding's cluster
+            self.clusters = self(embeddings)
 
             # update centroids based on mean of points in each cluster
             for i in range(self.n_clusters):
-                self.centroids[i] = np.mean(X[self.clusters == i], axis=0)
+                self.centroids[i] = np.mean(embeddings[self.clusters == i], axis=0)
 
             # check for convergence
             if np.array_equal(prev_clusters, self.clusters):
                 break
 
+            # enforce max iterations if needed
+            if max_iter and max_iter > 0:
+                max_iter -= 1
 
-    def predict(self, X):
+    def __call__(self, embeddings):
         # assign each point to closest centroid
-        clusters = np.zeros(X.shape[0])
-        for i, point in enumerate(X):
-            if type(point) != type(np.array([])):
+        clusters = np.zeros(embeddings.shape[0])
+        for i, point in enumerate(embeddings):
+            if not isinstance(point, np.ndarray):
                 point = point.toarray()
             distances = np.linalg.norm(point - self.centroids, axis=1)
             clusters[i] = np.argmin(distances)
@@ -50,37 +51,32 @@ class KMeans:
 
 def kmeans_cluster_and_evaluate(data_file, encoding_type):
     print(f'starting kmeans clustering and evaluation with {data_file} and encoding {encoding_type}')
-    data = {}
+
     # read in data
     with open(data_file, 'r') as f:
         reader = csv.reader(f, delimiter='\t')
-        data['sentences'] = [row[1] for row in reader]
+        labels, sentences = list(zip(*[row for row in reader]))
 
-    with open(data_file, 'r') as f:
-        reader = csv.reader(f, delimiter='\t')
-        data['labels'] = [row[0] for row in reader]
-
-        # extract features using TfidfVectorizer or SBERT
+    # extract features using TfidfVectorizer or SBERT
     if encoding_type == 'TFIDF':
-        vectorizer = TfidfVectorizer()
-        X = vectorizer.fit_transform(data['sentences'])
-    elif encoding_type == 'SBERT':
-        model_name = 'distilbert-base-nli-stsb-mean-tokens'
-        model = SentenceTransformer(model_name)
-        X = model.encode(data['sentences'])
+        embeddings = TfidfVectorizer().fit_transform(sentences)
+    else:
+        embeddings = SentenceTransformer('all-MiniLM-L12-v2').encode(sentences)
 
     # initialize KMeans with random centroids
-    model = KMeans(n_clusters=len(set(data['labels'])))
+    model = KMeans(n_clusters=len(set(labels)))
 
+    # calc scores
     RI_scores = []
     ARI_scores = []
+
     for i in range(10):
         # fit model to data
-        model.fit(X)
+        model.fit(embeddings)
 
         # evaluate model using RI and ARI
-        RI_scores.append(rand_score(data['labels'], model.predict(X)))
-        ARI_scores.append(adjusted_rand_score(data['labels'], model.predict(X)))
+        RI_scores.append(rand_score(labels, model(embeddings)))
+        ARI_scores.append(adjusted_rand_score(labels, model(embeddings)))
 
     # return mean evaluation scores
     evaluation_results = {'mean_RI_score': np.mean(RI_scores), 'mean_ARI_score': np.mean(ARI_scores)}
@@ -95,4 +91,4 @@ if __name__ == '__main__':
     results = kmeans_cluster_and_evaluate(config['data'], config["encoding_type"])
 
     for k, v in results.items():
-        print(k, v)
+        print(k, v, sep='\t')
